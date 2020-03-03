a5fc61222953c8bbbc807d100455dd45ed15bb74
Павел <46326271+iney17@users.noreply.github.com>
2020-03-03 23:32:06 +0200

    +slider

--- script.js
+++ script.js
@@ -40,3 +40,35 @@ function initializeClock(id, endtime) {
 
 let deadline = "2020-07-31";
 initializeClock("countdown", deadline);
+
+/*слайдер*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
